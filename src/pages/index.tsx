0f58ded85b6fff31513f866d6cc5e0388817a752
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2022-03-01 21:33:53 -0300

    Component Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Layout from "../components/Layout";
+import Tabela from '../components/Tabela';
+import Cliente from '../core/Cliente';
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente("Ana", 34, '1'),
+    new Cliente("Bia", 24, '2'),
+    new Cliente("Rayane", 27, '3'),
+    new Cliente("Vitoria", 22, '4'),
+  ]
+
   return (
     <div className={`
     flex justify-center items-center h-screen
@@ -8,7 +18,7 @@ export default function Home() {
     text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )

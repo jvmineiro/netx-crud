fd65a30f79ae969ad7a8b3fd36cb4ad9c0f11416
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2022-03-06 15:03:42 -0300

    Integrando Tabela e Formulario

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,27 +6,35 @@ import Tabela from '../components/Tabela';
 import Cliente from '../core/Cliente';
 
 export default function Home() {
-
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  
   const clientes = [
     new Cliente("Ana", 34, '1'),
     new Cliente("Bia", 24, '2'),
     new Cliente("Rayane", 27, '3'),
     new Cliente("Vitoria", 22, '4'),
   ]
-
+  
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome)
+    setCliente(cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido(cliente: Cliente) {
     console.log(`Excluir... ${cliente.nome}`)
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+    setVisivel('tabela')
+  }
+
 
   return (
     <div className={`
@@ -38,7 +46,10 @@ export default function Home() {
         {visivel === 'tabela' ? (
           <>
             <div className="flex justify-end">
-              <Botao cor="green" className="mb-4" onClick={() => setVisivel('form')}>Novo Cliente</Botao>
+              <Botao cor="green" className="mb-4" 
+                onClick={novoCliente}>
+                  Novo Cliente
+              </Botao>
             </div>
             <Tabela clientes={clientes} 
               clienteSelecionado={clienteSelecionado}
@@ -47,7 +58,7 @@ export default function Home() {
           </>
         ) : (
           <Formulario 
-            cliente={clientes[2]}
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')} 
           />

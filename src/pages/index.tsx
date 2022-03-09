23a01b6ed42ac2e2506c58be8a07f62790b445b5
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2022-03-08 21:02:29 -0300

    Tabela Integrada com Firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from '../components/Tabela';
 import Cliente from '../core/Cliente';
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 export default function Home() {
+
+  const repo: ClienteRepositorio = new ColecaoCliente() 
+
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
   
-  const clientes = [
-    new Cliente("Ana", 34, '1'),
-    new Cliente("Bia", 24, '2'),
-    new Cliente("Rayane", 27, '3'),
-    new Cliente("Vitoria", 22, '4'),
-  ]
-  
+  useEffect(obterTodos, [])
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
+
   function clienteSelecionado(cliente: Cliente) {
     setCliente(cliente)
     setVisivel('form')
@@ -30,9 +38,9 @@ export default function Home() {
     setVisivel('form')
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
-    setVisivel('tabela')
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
 
